b390c6769ecffa00b6fa822a4fdee483558118b3
booyaa <booyaabooyaabooyaa@gmail.com>
2016-07-22 08:24:38 +0100

    remove merge comments

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ fn main() {
     println!("Hello, world!");
     println!("Homu test");
     println!("hi from booyaadotorg");
-<<<<<<< HEAD
     println!("and another!");
-=======
     println!("another");
->>>>>>> 58c636b66e60ce4d7a3544eba5fab15d2b1b8e86
 }
